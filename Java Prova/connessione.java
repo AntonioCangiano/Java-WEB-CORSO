f98adee8f025b6f4bf95541b8e1ce1860e73ca6d
Antonio Cangiano <antonio.cangiano@mail.polimi.it>
2023-11-22 10:32:32 +0100

    conn. database

--- Java Prova/connessione.java
+++ Java Prova/connessione.java
@@ -11,60 +11,68 @@ public class connessione {
     public String DB_username = "root";
     public String DB_password ="root";
 
-
-
-    /**
-     * 
-     */
-    public void openConnection(){
-        Connection conn = null; 
-        PreparedStatement stmt = null;
-        ResultSet rs = null;
+    public Connection openConnection() {
+        Connection conn = null;
 
         try {
             Class.forName(DB_driver);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        try {
             conn = DriverManager.getConnection(DB_url, DB_username, DB_password);
-            if (conn!= null) {
-                System.out.println("connessione riuscita");
+            System.out.println("Connessione riuscita");
 
-                //Query Prova
-                String query=   "Insert into persona (nome) values ('prova')";
+            PreparedStatement stmt = null;
+            ResultSet rs = null;
+
+            // Query Prova
+            String query = "SELECT * FROM preside";
+
+            try {
                 stmt = conn.prepareStatement(query);
-
-                // Esecuzione della query
                 rs = stmt.executeQuery();
 
-            
                 // Elaborazione dei risultati
-                while (rs.next()) { 
+                while (rs.next()) {
                     // Recupera i dati dalla riga corrente
                     int id = rs.getInt("id");
-                    String nome = rs.getString("nome");
+                    String nome = rs.getString("nome_preside");
+                    String cognome = rs.getString("cognome_preside");
+                    String scuola = rs.getString("nome_scuola");
 
                     // Esempio di utilizzo dei dati
-                    System.out.println("ID: " + id + ", Nome: " + nome);
+                    System.out.println("ID: " + id );
+                    System.out.println("Nome: " + nome );
+                    System.out.println("Cognome: " + cognome );
+                    System.out.println("Scuola: " + scuola );
                 }
-                
-
-            }else{
-                System.out.println("connessione non riuscita");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        finally{
-            if (conn!=null) {
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
                 try {
-                    conn.close();
+                    if (stmt != null) {
+                        stmt.close();
+                    }
+                    if (rs != null) {
+                        rs.close();
+                    }
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
             }
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
+
+            return conn;
+    }
+
+    public void closeConnection(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+                System.out.println("Connessione chiusa");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
+
 }
